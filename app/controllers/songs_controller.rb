9ab7e20aaae20f0d6cf37502a1bc8121f57a85ac
sophie <justanothersophie@gmail.com>
2021-09-10 19:40:27 -0400

    commit tree 45d9c6ca11b4a17da032d77952da4ebf40a4428c

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,28 @@
 class SongsController < ApplicationController
+
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      if Artist.find_by(id: params[:artist_id])
+        @songs = Song.all_by_artist_id(params[:artist_id])
+      else
+        flash[:alert] = "Artist not found."
+        redirect_to artists_path
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    @song = Song.find_by(id: params[:id])
+    if !@song
+      flash[:alert] = "Song not found."
+      if params[:artist_id]
+        redirect_to artist_songs_path(params[:artist_id])
+      else
+        redirect_to songs_path
+      end
+    end
   end
 
   def new
